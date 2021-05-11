41e7a63b5715c379131e26cf3492978c554caf98
samchencode <samchencode@gmail.com>
2021-05-11 17:09:32 -0400

    fix: extra padding around 'new' button

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -19,6 +19,20 @@ function App() {
             </div>
           </li>
           <li className="pattern-input__row">
+            <label htmlFor="pattern--1">ReGex Pattern 1</label>
+            <div className="pattern-input__input-group">
+              <input type="text" id="pattern--1" placeholder="/.*/g" />
+              <button>Remove</button>
+            </div>
+          </li>
+          <li className="pattern-input__row">
+            <label htmlFor="pattern--1">ReGex Pattern 1</label>
+            <div className="pattern-input__input-group">
+              <input type="text" id="pattern--1" placeholder="/.*/g" />
+              <button>Remove</button>
+            </div>
+          </li>
+          <li className="pattern-input__row pattern-input__row--full">
             <button className="pattern-input__new">New</button>
           </li>
         </ul>
