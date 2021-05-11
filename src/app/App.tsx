c45847fa1156fbc1f4063feba4700979dba003de
samchencode <samchencode@gmail.com>
2021-05-11 15:02:09 -0400

    feat: basic markup for mobile

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,8 +3,44 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="app">
+      {/* eslint-disable-next-line no-script-url */}
+      <form className="pattern-input" action="javascript:void(0);">
+        <div className="pattern-input__row">
+          <label htmlFor="pattern--1" >ReGex Pattern 1</label>
+          <input type="text" id="pattern--1" placeholder="/.*/g" />
+        </div>
+        <div className="pattern-input__row">
+          <label htmlFor="pattern--2" >ReGex Pattern 2</label>
+          <input type="text" id="pattern--2" placeholder="/.*/g" />
+        </div>
+        <div className="pattern-input__row">
+          <label htmlFor="pattern--3" >ReGex Pattern 3</label>
+          <input type="text" id="pattern--3" placeholder="/.*/g" />
+        </div>
+        <div className="pattern-input__row">
+          <label htmlFor="pattern--4" >ReGex Pattern 3</label>
+          <input type="text" id="pattern--4" placeholder="/.*/g" />
+        </div>
+        <button className="pattern-input__new">New Rule</button>
+      </form>
+
+      <div className="text-display">
+        <textarea className="text-display__input">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, vero.
+        </textarea>
+        <textarea className="text-display__highlight-overlay">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, vero.
+        </textarea>
+      </div>
+
+      <nav className="bottom-nav">
+        <ul className="bottom-nav__list">
+          <li className="bottom-nav__item">Input</li>
+          <li className="bottom-nav__item">List</li>
+          <li className="bottom-nav__item">Replace</li>
+        </ul>
+      </nav>
     </div>
   );
 }
