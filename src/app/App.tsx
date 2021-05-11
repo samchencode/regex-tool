7e093cee265e9bcb0938e87bf15ee7f6ac221108
samchencode <samchencode@gmail.com>
2021-05-11 15:45:56 -0400

    feat: fading bottom of list

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,31 +6,37 @@ function App() {
     <div className="app">
       {/* eslint-disable-next-line no-script-url */}
       <form className="pattern-input" action="javascript:void(0);">
-        <div className="pattern-input__row">
-          <label htmlFor="pattern--1" >ReGex Pattern 1</label>
-          <input type="text" id="pattern--1" placeholder="/.*/g" />
-        </div>
-        <div className="pattern-input__row">
-          <label htmlFor="pattern--2" >ReGex Pattern 2</label>
-          <input type="text" id="pattern--2" placeholder="/.*/g" />
-        </div>
-        <div className="pattern-input__row">
-          <label htmlFor="pattern--3" >ReGex Pattern 3</label>
-          <input type="text" id="pattern--3" placeholder="/.*/g" />
-        </div>
-        <div className="pattern-input__row">
-          <label htmlFor="pattern--4" >ReGex Pattern 3</label>
-          <input type="text" id="pattern--4" placeholder="/.*/g" />
-        </div>
-        <button className="pattern-input__new">New Rule</button>
+      <ul className="pattern-input__list">
+          <li className="pattern-input__row">
+            <label htmlFor="pattern--1">ReGex Pattern 1</label>
+            <input type="text" id="pattern--1" placeholder="/.*/g" />
+          </li>
+          <li className="pattern-input__row">
+            <label htmlFor="pattern--2">ReGex Pattern 2</label>
+            <input type="text" id="pattern--2" placeholder="/.*/g" />
+          </li>
+          <li className="pattern-input__row">
+            <label htmlFor="pattern--3">ReGex Pattern 3</label>
+            <input type="text" id="pattern--3" placeholder="/.*/g" />
+          </li>
+          <li className="pattern-input__row">
+            <label htmlFor="pattern--4">ReGex Pattern 4</label>
+            <input type="text" id="pattern--4" placeholder="/.*/g" />
+          </li>
+          <li>
+            <button className="pattern-input__new">New Rule</button>
+          </li>
+        </ul>
       </form>
 
       <div className="text-display">
         <textarea className="text-display__input">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, vero.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum,
+          vero.
         </textarea>
         <textarea className="text-display__highlight-overlay">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, vero.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum,
+          vero.
         </textarea>
       </div>
 
