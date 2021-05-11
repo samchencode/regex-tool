72f0d97b98eb126ad322822565224b0c61acbe7f
samchencode <samchencode@gmail.com>
2021-05-11 17:13:49 -0400

    fix: remove extra space around text-display__label

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -39,7 +39,7 @@ function App() {
       </form>
 
       <div className="text-display__container">
-        <h3>Input</h3>
+        <h3 className="text-display__label">Input</h3>
         <div className="text-display">
           <textarea className="text-display__input" placeholder="Input text to search...">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum,
