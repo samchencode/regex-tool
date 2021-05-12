abe5ae108a6c7f9b36ac58bcdc07edfbac60f408
samchencode <samchencode@gmail.com>
2021-05-12 09:16:38 -0400

    feat: handle notfound /match/* paths

--- src/features/dashboard/routes.tsx
+++ src/features/dashboard/routes.tsx
@@ -3,12 +3,15 @@ import { Route, Switch } from 'react-router-dom';
 import HomeScreen from './HomeScreen';
 import ListMatchesScreen from './ListMatchesScreen';
 import ReplaceScreen from './ReplaceScreen';
+import NotFoundScreen from '../not-found';
+
 
 const Routes = () => (
   <Switch>
     <Route exact path="/match/" component={HomeScreen} />
     <Route path="/match/list" component={ListMatchesScreen} />
     <Route path="/match/replace" component={ReplaceScreen} />
+    <Route component={NotFoundScreen} />
   </Switch>
 );
 
