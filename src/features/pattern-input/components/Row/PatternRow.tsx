aa8a17a5c726736dca61e124d9e417b930a6dbc2
samchencode <samchencode@gmail.com>
2021-05-12 07:48:22 -0400

    feat: conditional rendering operation pattern row input

--- src/features/pattern-input/components/Row/PatternRow.tsx
+++ src/features/pattern-input/components/Row/PatternRow.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 
-const Pattern = () => (
+enum PatternRowType {
+  patternOnly,
+  patternList,
+  patternReplace
+}
+
+interface PatternRowProps {
+  type: PatternRowType;
+}
+
+const Pattern = ({ type }: PatternRowProps) => (
   <li className="pattern-input__row">
     <div className="pattern-input__input-group pattern-input__input-group--match">
       <label htmlFor="match-pattern-1">ReGex Match 1</label>
       <input type="text" id="match-pattern-1" placeholder="/.*/g" />
     </div>
-    <div className="pattern-input__input-group pattern-input__input-group--operation">
+    { type !== PatternRowType.patternOnly && <div className="pattern-input__input-group pattern-input__input-group--operation">
       <label htmlFor="operation-pattern-1">ReGex Operation 1</label>
       <input type="text" id="operation-pattern-1" placeholder="$&" />
-    </div>
+    </div>}
     <button className="button--transparent"><i className="fas fa-times"></i></button>
   </li>
 );
 
 export default Pattern;
+export type { PatternRowType };
