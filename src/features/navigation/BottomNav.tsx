eba5054e551edef0101bebc5e83805020be6ab2f
samchencode <samchencode@gmail.com>
2021-05-12 06:42:28 -0400

    feat: add link func to navs

--- src/features/navigation/BottomNav.tsx
+++ src/features/navigation/BottomNav.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 import './BottomNav.css';
 
 const BottomNav = () => (
+  <Route exact path={['/', '/list', '/replace']}>
   <nav className="bottom-nav">
     <ul className="bottom-nav__list">
       <li className="bottom-nav__item">
+        <Link to="/">
         <i className="bottom-nav__icon far fa-file-alt"></i>
         <div className="bottom-nav__label">Input</div>
+        </Link>
       </li>
       <li className="bottom-nav__item">
+        <Link to="/list">
         <i className="bottom-nav__icon fas fa-clipboard-list"></i>
         <div className="bottom-nav__label">List</div>
+        </Link>
       </li>
       <li className="bottom-nav__item">
+        <Link to="/replace">
         <i className="bottom-nav__icon fas fa-random"></i>
         <div className="bottom-nav__label">Replace</div>
+        </Link>
       </li>
     </ul>
   </nav>
+  </Route>
 );
 
 export default BottomNav;
--- src/features/navigation/TopNav.tsx
+++ src/features/navigation/TopNav.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
 import './TopNav.css';
 
 const TopNav = () => (
@@ -7,8 +8,14 @@ const TopNav = () => (
       <li className="top-nav__item top-nav__item--title">
         <h1 className="top-nav__title">samchencode</h1>
       </li>
-      <li className="top-nav__item">List Matches</li>
-      <li className="top-nav__item">Replace</li>
+      <Route exact path={['/', '/list', '/replace']}>
+        <li className="top-nav__item">
+          <Link to="/list">List Matches</Link>
+        </li>
+        <li className="top-nav__item">
+          <Link to="/replace">Replace</Link>
+        </li>
+      </Route>
     </ul>
   </nav>
 );
