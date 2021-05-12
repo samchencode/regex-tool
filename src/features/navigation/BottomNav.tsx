3556922c86bde1e64f302f7888f9eaed67c74449
samchencode <samchencode@gmail.com>
2021-05-12 07:49:11 -0400

    format: bottom-nav

--- src/features/navigation/BottomNav.tsx
+++ src/features/navigation/BottomNav.tsx
@@ -4,28 +4,28 @@ import './BottomNav.css';
 
 const BottomNav = () => (
   <Route exact path={['/', '/list', '/replace']}>
-  <nav className="bottom-nav">
-    <ul className="bottom-nav__list">
-      <li className="bottom-nav__item">
-        <Link to="/">
-        <i className="bottom-nav__icon far fa-file-alt"></i>
-        <div className="bottom-nav__label">Input</div>
-        </Link>
-      </li>
-      <li className="bottom-nav__item">
-        <Link to="/list">
-        <i className="bottom-nav__icon fas fa-clipboard-list"></i>
-        <div className="bottom-nav__label">List</div>
-        </Link>
-      </li>
-      <li className="bottom-nav__item">
-        <Link to="/replace">
-        <i className="bottom-nav__icon fas fa-random"></i>
-        <div className="bottom-nav__label">Replace</div>
-        </Link>
-      </li>
-    </ul>
-  </nav>
+    <nav className="bottom-nav">
+      <ul className="bottom-nav__list">
+        <li className="bottom-nav__item">
+          <Link to="/">
+            <i className="bottom-nav__icon far fa-file-alt"></i>
+            <div className="bottom-nav__label">Input</div>
+          </Link>
+        </li>
+        <li className="bottom-nav__item">
+          <Link to="/list">
+            <i className="bottom-nav__icon fas fa-clipboard-list"></i>
+            <div className="bottom-nav__label">List</div>
+          </Link>
+        </li>
+        <li className="bottom-nav__item">
+          <Link to="/replace">
+            <i className="bottom-nav__icon fas fa-random"></i>
+            <div className="bottom-nav__label">Replace</div>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   </Route>
 );
 
