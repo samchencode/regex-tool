f95e32ce84a3860df16b7e52af5910b2339e90a8
samchencode <samchencode@gmail.com>
2021-05-13 17:41:04 -0400

    feat: highlight overlay mirrors scrolling

--- src/features/text-display/TextDisplay.tsx
+++ src/features/text-display/TextDisplay.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './style.css';
 
 enum TextDisplayType {
@@ -12,6 +12,14 @@ interface TextDisplayProps {
 
 const TextDisplay = (props: TextDisplayProps) => {
   const isInput = props.type === TextDisplayType.INPUT;
+  const overlayRef = useRef(null);
+  const textAreaRef = useRef(null);
+
+  const handleScrollTextArea = () => {
+    const overlay = overlayRef.current as unknown as HTMLDivElement;
+    const textArea = textAreaRef.current as unknown as HTMLTextAreaElement;
+    overlay.scrollTo(textArea.scrollLeft, textArea.scrollTop);
+  }
 
   return (
     <div
@@ -23,7 +31,7 @@ const TextDisplay = (props: TextDisplayProps) => {
       <h3 className="text-display__label">{isInput ? 'Input' : 'Output'}</h3>
       <div className="text-display__content">
         {isInput && (
-          <div className="text-display__highlight-overlay">
+          <div className="text-display__highlight-overlay" ref={overlayRef}>
             <mark>Lorem ipsum dolor</mark> sit amet consectetur adipisicing
             elit. Quod, nesciunt? A consequatur, porro at <mark>ex quod</mark>{' '}
             hic placeat non amet?
@@ -31,8 +39,10 @@ const TextDisplay = (props: TextDisplayProps) => {
         )}
         <textarea
           className="text-display__top"
+          ref={textAreaRef}
           placeholder="Input text to search..."
           readOnly={!isInput}
+          onScroll={isInput ? handleScrollTextArea : undefined}
           onChange={e => null}
           value="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, nesciunt?
           A consequatur, porro at ex quod hic placeat non amet?"
