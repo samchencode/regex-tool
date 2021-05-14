31ecad7da696c2882c2ee62f94411bdc4006c22c
samchencode <samchencode@gmail.com>
2021-05-14 10:56:54 -0400

    feat: export pattern selectors

--- src/features/pattern-input/index.ts
+++ src/features/pattern-input/index.ts
@@ -1,3 +1,25 @@
+import {
+  selectOperations,
+  selectOperationIds,
+  selectOperation,
+  selectOperationPattern,
+  selectOperationFlags,
+  selectOperationListFormat,
+  selectOperationReplace,
+  selectOperationColor,
+} from './patternSlice';
+
 export { default } from './PatternInput';
 export { PatternRowType } from './components/Row';
 export { default as patternReducer } from './patternSlice';
+
+export const patternSelectors = {
+  selectOperations,
+  selectOperationIds,
+  selectOperation,
+  selectOperationPattern,
+  selectOperationFlags,
+  selectOperationListFormat,
+  selectOperationReplace,
+  selectOperationColor,
+};
