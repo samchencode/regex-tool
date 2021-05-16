e81ea3612fc52bea8acf91a29f216d650c05b10a
samchencode <samchencode@gmail.com>
2021-05-16 08:28:22 -0400

    style: prettier

--- src/features/text-display/TextDisplayContainer.tsx
+++ src/features/text-display/TextDisplayContainer.tsx
@@ -23,24 +23,26 @@ const TextDisplayContainer = ({ type }: TextDisplayContainerProps) => {
   const dispatch = useAppDispatch();
 
   const input = useAppSelector(selectInput);
-  const formatter = type === TextDisplayType.LIST ? regex.list : regex.replace;
 
   const ops = useAppSelector(selectOperations);
   const results = transform({
     input,
-    transforms: ops.map(op => ({
-      formatter,
+    transforms: ops.map((op) => ({
+      formatter: type === TextDisplayType.LIST ? regex.list : regex.replace,
       pattern: op.pattern,
       flags: op.flags,
       format: type === TextDisplayType.LIST ? op.listFormat : op.replace,
-    }))
+    })),
   });
 
   const output = results[results.length - 1]?.result ?? input;
 
   return (
     <div className="text-display__container">
-      <InputTextDisplay value={input} onChange={(v) => dispatch(setInput({value: v}))} />
+      <InputTextDisplay
+        value={input}
+        onChange={(v) => dispatch(setInput({ value: v }))}
+      />
       {type !== TextDisplayType.INPUT_ONLY && (
         <OutputTextDisplay value={output} />
       )}
