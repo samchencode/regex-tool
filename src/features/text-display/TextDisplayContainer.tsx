6acf30866360139175c31e90377badb6927838e1
samchencode <samchencode@gmail.com>
2021-05-16 08:23:51 -0400

    feat: run multiple regex in sequence

--- src/features/text-display/TextDisplayContainer.tsx
+++ src/features/text-display/TextDisplayContainer.tsx
@@ -3,7 +3,7 @@ import { OutputTextDisplay, InputTextDisplay } from './components/Display';
 import './style.css';
 
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
-import { patternSelectors, patternActions } from '../pattern';
+import { patternSelectors, patternActions, transform } from '../pattern';
 import * as regex from '../../lib/regex';
 
 const { selectOperations, selectInput } = patternSelectors;
@@ -23,23 +23,26 @@ const TextDisplayContainer = ({ type }: TextDisplayContainerProps) => {
   const dispatch = useAppDispatch();
 
   const input = useAppSelector(selectInput);
-  const { pattern, flags, listFormat, replace } =
-    useAppSelector(selectOperations)[0];
-  const format = type === TextDisplayType.LIST ? listFormat : replace;
   const formatter = type === TextDisplayType.LIST ? regex.list : regex.replace;
 
-  let result: string;
-  try {
-    result = formatter(pattern, flags, input, format);
-  } catch (e) {
-    result = e.message;
-  }
+  const ops = useAppSelector(selectOperations);
+  const results = transform({
+    input,
+    transforms: ops.map(op => ({
+      formatter,
+      pattern: op.pattern,
+      flags: op.flags,
+      format: type === TextDisplayType.LIST ? op.listFormat : op.replace,
+    }))
+  });
+
+  const output = results[results.length - 1]?.result ?? input;
 
   return (
     <div className="text-display__container">
       <InputTextDisplay value={input} onChange={(v) => dispatch(setInput({value: v}))} />
       {type !== TextDisplayType.INPUT_ONLY && (
-        <OutputTextDisplay value={result} />
+        <OutputTextDisplay value={output} />
       )}
     </div>
   );
