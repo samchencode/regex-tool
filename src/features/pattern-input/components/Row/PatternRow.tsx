d1e46a5e9f188dd0d4939bc93aeb490fba65f241
samchencode <samchencode@gmail.com>
2021-05-16 20:35:12 -0400

    fix: stop propagation of button click

--- src/features/pattern-input/components/Row/PatternRow.tsx
+++ src/features/pattern-input/components/Row/PatternRow.tsx
@@ -86,6 +86,7 @@ const Pattern = ({
         className="button--transparent"
         onClick={(e) => {
           e.preventDefault();
+          e.stopPropagation();
           onClickRemove();
         }}
       >
